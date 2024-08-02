4614a552fc4296e4cd21f1dfa5a952201aed57c3
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-08-02 13:26:56 +0500

    add saving characters by localStorage

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-function addCharacter() {
+if (localStorage.getItem("counting") === null) {
+    localStorage.setItem("counting", "0");
+}
+
+for (var i = 0; i < parseInt(localStorage.getItem("counting")); ++i) {
     const character = document.createElement('div');
     character.classList.add("charapter-box");
     document.querySelector("main").appendChild(character);
 }
 
+function addCharacter() {
+    const character = document.createElement('div');
+    character.classList.add("charapter-box");
+    document.querySelector("main").appendChild(character);
+
+    localStorage.setItem("counting", (parseInt(localStorage.getItem("counting")) + 1).toString());
+}
+
 document.querySelector(".add-char-btn").addEventListener("click", () =>
     addCharacter()
 );
+
