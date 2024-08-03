929c0fe708eaef206ea8a57f529c11b78a88946d
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-08-03 14:19:40 +0500

    added health btn

--- js/battle.js
+++ js/battle.js
@@ -134,16 +134,42 @@ function clearCharacters() {
 function openCharInfo(id) {
     if (document.querySelector(".char-addinfo-container") != null) {
         if (document.querySelector(".char-addinfo-container").id == id + "-info") {
+            document.querySelector("#" + id).children[1].children[1].classList.remove("info-btn-opened");
             document.querySelector(".char-addinfo-container").remove();
             return;
         }
+        let last_id = document.querySelector(".char-addinfo-container").id;
+        last_id = last_id.substring(0, last_id.length - 5);
+
+        document.querySelector("#" + last_id).children[1].children[1].classList.remove("info-btn-opened");
         document.querySelector(".char-addinfo-container").remove();
     }
 
     const info = document.createElement('div');
     info.classList.add("char-addinfo-container");
     info.id = id + "-info";
-    info.innerHTML = '<div id="char-addinfo"></div>';
+
+    const box = document.createElement('div');
+    box.id = "char-addinfo";
+
+    const health_btn = document.createElement('button');
+    health_btn.id = "health-btn";
+    health_btn.classList.add("add-info-btn");
+    health_btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" ' +
+                                    'class="bi bi-heart" viewBox="0 0 16 16">' +
+                                '<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 ' +
+                                    ' 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 ' +
+                                    ' 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 ' +
+                                    ' 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 ' +
+                                    ' 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>' +
+                            '</svg>' +
+                            '<p>Health</p>'
+
+    box.appendChild(health_btn);
+
+    info.appendChild(box);
+
+    document.querySelector("#" + id).children[1].children[1].classList.add("info-btn-opened");
 
     document.querySelector("#" + id).insertAdjacentElement("afterend", info);
 }
